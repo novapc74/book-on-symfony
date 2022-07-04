bf7f3ac9730de9c1fe682475cb9b9cf2856e6e0c
novapc74 <novapc@ukr.net>
2022-07-04 15:38:28 +0300

    add simple emailer to MassageController

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -9,12 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\MessageRepository;
-use VictorPrdh\ReCaptchaBundle\Form\ReCaptchaType;
+// use VictorPrdh\ReCaptchaBundle\Form\ReCaptchaType;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class MessageController extends AbstractController
 {
     #[Route(path: '/message/create', methods: ["GET", "POST"], name: 'message_create')]
-    public function create(MessageRepository $messageRepository, Request $request): Response
+    public function create(MailerInterface $mailer, MessageRepository $messageRepository, Request $request): Response
     {
         $message = new Message();
 
@@ -23,6 +25,15 @@ class MessageController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $messageRepository->add($message, true);
+
+            $email = (new Email())
+                ->from('symfony_book@service.inf')
+                ->to($message->getEmail())
+                ->subject('Your message id delivered')
+                ->text('We are allredy reading your message!')
+                ->html('<p>Here is a <span><h3>beautiful</h3></span> description wrapped in html</p>');
+
+                $mailer->send($email);
 
             $this->addFlash(
                 'success',
