0a4b26d59c134600ea7bffa213d1cbbe1e26462c
novapc74 <novapc@ukr.net>
2022-07-04 13:08:25 +0300

    fix titles in tamplates

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,8 +5,6 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 #[Route(path:'/admin')]
 class DashboardController extends AbstractController
